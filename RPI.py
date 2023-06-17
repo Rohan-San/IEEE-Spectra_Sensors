3cec07e0d520afc28bcd63038131dc5db8b1970c
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2023-06-17 12:37:03 +0530

    Formatting

--- RPI.py
+++ RPI.py
@@ -10,20 +10,19 @@ def readIR(pin):
     GPIO.SETUP(pin, GPIO.OUT)
     GPIO.output(pin, False)
     time.sleep(0.1)
-
     GPIO.setup(pin, GPIO.IN)
     i = GPIO.input(pin)
     if i == False:
-        print("No Inturuders !", i)
+        print("No Inturuders!", i)
         GPIO.output(LIGHT_PIN, False)
     elif (i == True):
-        print("intruder alert !", i)
-        GPIO.OUTPUT(LIGHT_PIN, True)
+        print("Intruder Alert!", i)
+        GPIO.output(LIGHT_PIN, True)
 try:
     while True:
         readIR(IR_PIN)
         time.sleep(0.5)
-except(KeyboardInterrupt):
+except KeyboardInterrupt:
     GPIO.cleanup()
     exit()
     
